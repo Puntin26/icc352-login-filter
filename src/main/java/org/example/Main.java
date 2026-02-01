fc726e3f617325349e69c00532ad0cc799cc05a1
Ian Alvarez <iaag0001@ce.pucmm.edu.do>
2026-02-01 14:04:33 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,67 @@
+package org.example;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+public class Main {
+    record Usuario(String usuario, String contrasena){
+
+    }
+
+    static void main(){
+        var app = Javalin.create(config -> {
+            //configurando los documentos estaticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/publico";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress=false;
+                staticFileConfig.aliasCheck=null;
+            });
+        });
+
+            //start server on port 7000
+            app.start(7000);
+
+            //app
+            app.before(/* "/", */ctx -> {
+
+                if(ctx.path().equals("/login.html") || ctx.path().equals("/procesarlogin")){
+                    return;
+                }
+
+                System.out.println("Before en el endpoint /");
+                Usuario usuario = ctx.sessionAttribute("usuario");
+                if (usuario == null) {
+                    ctx.redirect("/login.html");
+                } else {
+                    System.out.println("Usuario en sesion: " + usuario.usuario);
+                }
+            });
+
+            //end point hola mundo
+            app.get("/", ctx -> {
+                ctx.result("Hello desde javalin");
+            });
+
+            app.post("/procesarlogin", ctx -> {
+                String usuario = ctx.formParam("nombre");
+                String contrasena = ctx.formParam("contrasena");
+                //
+                if (usuario.equals("admin") && contrasena.equals("1234")) {
+                    ctx.sessionAttribute("usuario", new Usuario(usuario, contrasena));
+                }//
+                ctx.redirect("/");
+            });
+
+            app.get("/otro-endpoint", ctx -> {
+                ctx.result("Hello desde otro endpoint");
+            });
+
+            app.get("/logout", ctx -> {
+                ctx.req().getSession().invalidate();
+                ctx.redirect("/login.html");
+            });
+
+    }
+}
